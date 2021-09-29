828a6ff86616693eb42c8b629040b5c4a6e17dca
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-29 16:49:31 +0900

    add: タイトルを追加

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>イマレピ</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <p>aiueo</p>
+        
       </header>
     </div>
   );
