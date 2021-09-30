e01b07083af6e48afd4b34985a5e30b03d399054
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-30 13:45:00 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import liff from '@line/liff';
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-// import * as Config from '../config';
+import React from 'react';
+import liff from '@line/liff'; // 追加
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-
-  useEffect(()=>{
-      initLiff()// 初期化処理
-  }, [])
-
-  /**
-   * LIFFの初期化を行う
-   * 初期化完了. 以降はLIFF SDKの各種機能を利用できる
-   *  =>初期化前でも使用できる機能もある（liff.isInClient()など）
-   */
-  const initLiff = () => {
-      liff.init({ liffId: process.env.REACT_APP_LIFF_ID})
-          .then(()=>{ 
-              //ログインしていなければログインさせる
-              if(liff.isLoggedIn() === false) liff.login({})
-          }).catch( (error)=> {});
+  /* 追加: メッセージ送信 */
+  const sendMessage = () => {
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID}) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) { // LIFFので動いているのであれば
+          liff.sendMessages([{ // メッセージを送信する
+            'type': 'text',
+            'text': "You've successfully sent a message! Hooray!"
+          }]).then(function() {
+            window.alert('Message sent');
+          }).catch(function(error) {
+            window.alert('Error sending message: ' + error);
+          });
+        }
+      })
   }
 
-  /**
-   * LINEで保持しているユーザー情報取得
-   */
+  /* 追加: UserProfileをAlertで表示 */
   const getUserInfo = () => {
-      liff.getProfile().then(profile => {
-          alert( JSON.stringify(profile) );
-      }).catch((error)=>{})
+    liff.init({liffId: process.env.REACT_APP_LIFF_ID})
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+        } else if (liff.isInClient()) {
+          liff.getProfile()  // ユーザ情報を取得する
+            .then(profile => {
+              const userId = profile.userId
+              const displayName = profile.displayName
+              alert(`Name: ${displayName}, userId: ${userId}`)
+            }).catch(function(error) {
+              window.alert('Error sending message: ' + error);
+            });
+        }
+      })
+
   }
-  const sendMessege = () => {
-    liff.sendMessages([{
-      'type':'text',
-      'text':'送信テストです',
-    }])
-  }
+
   return (
-      <div>
-          {/* LIFF内以外からアクセス */}
-          {liff.isInClient() === false ?
-              <p>ブラウザからはお使いいただけません。LINE内アプリ（LIFF）からご利用ください。</p>
-          :
-              <p>こんにちは</p>
-          }
-          {getUserInfo}
-          <button onClick={sendMessege}>メッセージを送信</button>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <button className="button" onClick={sendMessage}>send message</button> // 追加
+        <button className="button" onClick={getUserInfo}>show user info</button> // 追加
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
