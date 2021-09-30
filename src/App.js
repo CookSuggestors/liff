e8e876175906532e8052ae9c13f07d53b0654ea0
YamadaKouki12 <215x022x@stu.kobe-u.ac.jp>
2021-09-30 13:38:00 +0900

    送信テスト

--- src/App.js
+++ src/App.js
@@ -5,43 +5,50 @@ import ReactDOM from 'react-dom';
 
 function App() {
 
-    useEffect(()=>{
-        initLiff()// 初期化処理
-    }, [])
 
-    /**
-     * LIFFの初期化を行う
-     * 初期化完了. 以降はLIFF SDKの各種機能を利用できる
-     *  =>初期化前でも使用できる機能もある（liff.isInClient()など）
-     */
-    const initLiff = () => {
-        liff.init({ liffId: process.env.REACT_APP_LIFF_ID})
-            .then(()=>{ 
-                //ログインしていなければログインさせる
-                if(liff.isLoggedIn() === false) liff.login({})
-            }).catch( (error)=> {});
-    }
+  useEffect(()=>{
+      initLiff()// 初期化処理
+  }, [])
 
-    /**
-     * LINEで保持しているユーザー情報取得
-     */
-    const getUserInfo = () => {
-        liff.getProfile().then(profile => {
-            alert( JSON.stringify(profile) );
-        }).catch((error)=>{})
-    }
+  /**
+   * LIFFの初期化を行う
+   * 初期化完了. 以降はLIFF SDKの各種機能を利用できる
+   *  =>初期化前でも使用できる機能もある（liff.isInClient()など）
+   */
+  const initLiff = () => {
+      liff.init({ liffId: process.env.REACT_APP_LIFF_ID})
+          .then(()=>{ 
+              //ログインしていなければログインさせる
+              if(liff.isLoggedIn() === false) liff.login({})
+          }).catch( (error)=> {});
+  }
 
-    return (
-        <>
-            {/* LIFF内以外からアクセス */}
-            {liff.isInClient() === false ?
-                <p>ブラウザからはお使いいただけません。LINE内アプリ（LIFF）からご利用ください。</p>
-            :
-                <p>こんにちは</p>
-            }
-            {getUserInfo}
-        </>
-    );
+  /**
+   * LINEで保持しているユーザー情報取得
+   */
+  const getUserInfo = () => {
+      liff.getProfile().then(profile => {
+          alert( JSON.stringify(profile) );
+      }).catch((error)=>{})
+  }
+  const sendMessege = () => {
+    liff.sendMessages([{
+      'type':'text',
+      'text':'送信テストです',
+    }])
+  }
+  return (
+      <div>
+          {/* LIFF内以外からアクセス */}
+          {liff.isInClient() === false ?
+              <p>ブラウザからはお使いいただけません。LINE内アプリ（LIFF）からご利用ください。</p>
+          :
+              <p>こんにちは</p>
+          }
+          {getUserInfo}
+          <button onClick={sendMessege}>メッセージを送信</button>
+      </div>
+  );
 }
 
 export default App;
